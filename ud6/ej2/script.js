a940f8d769bbe9536fa730d5ac9b8bb5848b513a
AdriaMelero1 <adriamelero1@gmail.com>
2024-01-18 20:13:01 +0100

    Memory game finished

--- ud6/ej2/script.js
+++ ud6/ej2/script.js
@@ -11,34 +11,46 @@ $(document).ready(function () {
 
     cards.sort(() => Math.random() - 0.5);
 
+    $("body").append("<div class='message'></div>");
     $("body").prepend("<div class='container'></div>");
-    $("body").prepend("<div class='message'></div>");
+    $("body").append("<button>RESTART</button>");
 
     for (let i = 0; i < cards.length; i++) {
         $(".container").prepend(`<div class='card'><div class='num'>${cards[i]}</div></div>`);
     }
 
+
+    $("button").on("click", function () {
+        location.reload();
+    });
+
     $(".card").on("click", function () {
         console.log("object");
 
-        if (showedCards < 2 && !$(this).children().hasClass("show")) {
-            $(this).children().css("opacity", "1");
+        if (showedCards < 2 & !$(this).children().hasClass("show")) {
             showedCards++;
+            $(this).children().css("opacity", "1");
 
-            n.push($(this).children().text());
+            n.push($(this));
 
             if (showedCards == 2) {
-                if (n[0] == n[1]) {
-									$(".message").text("You found a pair!");
-									$(".card:contains('" + n[0] + "')").children().addClass("show");
-                } else {
-									$(".message").text("Not a pair");
+                if (n[0].children().text() == n[1].children().text()) {
+                    $(".message").text("You found a pair!");
+                    $(n[0]).addClass("show");
+                    $(n[1]).addClass("show");
 
-                    
+                    if($(".card:not(.show)").length == 0){
+                        $(".message").text("You won!");
+
+                    }
+                } else {
+                    $(".message").text("Not a pair");
+
                     setTimeout(() => {
-                        $(".card:not(.show)").children().css("opacity", "1");
-                        $(".card").children().css("opacity", "0");
-												$(".message").text("");
+                        $(".card:not(.show)").children().css("opacity", "0");
+                        $(n[0]).children().css("opacity", "1");
+                        $(n[1]).children().css("opacity", "1");
+                        $(".message").text("");
 
                     }, 1000);
                 }
