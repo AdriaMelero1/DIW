d5745858b49fadfaa6d6c461d157863db6de90bb
AdriaMelero1 <adriamelero1@gmail.com>
2024-01-23 19:41:22 +0100

    Post its: containers count postits by color, post its generated, open and close.

--- ud6/ej4/script.js
+++ ud6/ej4/script.js
@@ -1,42 +1,103 @@
-$("body").append("<div class='blue-container droppable'><p class='counter'></p></div>");
-$("body").append("<div class='red-container droppable'></div>");
-$("body").append("<div class='yellow-container droppable'></div>");
+$("body").append("<div class='blue-container'><span>0</span></div>");
+$("body").append("<div class='red-container'><span>0</span></div>");
+$("body").append("<div class='yellow-container'><span>0</span></div>");
 
-// $("body").append("<div class='draggable blue'></div>");
-// $("body").append("<div class='draggable adria'></div>");
 $("body").append("<button id='new'>NEW POST IT</button>");
 
-
+//CREATION OF POSTITS (random color)
 $("#new").click(function () {
-	$("body").append("<div class='postit'>x</div>");
-	$(".postit").draggable();
+	let colorIndex = Math.floor(Math.random() * 3);
+	let colors = ["blue", "red", "yellow"];
+
+	let postit = $("<div class='postit'><div class='close'>x</div><div class='text'>kajsdkjasdjkasd</div></div>");
+
+	$("body").append(postit)
+
+	postit.draggable().data("dropped", false).addClass(colors[colorIndex]);
+
 });
 
-$(".draggable").draggable();
+
+//OPEN POST IT
+$("body").on("click", ".postit:not(.activated)", function () {
+	$(this).addClass("activated");
+	$(this).draggable("disable");
+});
 
 
-$(".droppable").droppable({
-	drop: function (e, ui) {
 
-		
-		if ($(this).hasClass("blue-container")) {
-			
-			$('.blue-container').text("Container is blue");
-		}
+//CLOSE POST IT
+$("body").on("click", ".close", function () {
+	if ($(this).parent().hasClass("activated")) {
+		$(this).parent().removeClass("activated");
+		$(this).parent().draggable("enable");
+	}
+});
 
-		if (ui.draggable.hasClass("blue")) {
-			$('.blue-container').text("Draggable has class blue");
-		}
 
-		let counter = 0;
 
-		ui.draggable.each(function () {
+
+//CONTAINER DROPPABLES
+
+$(".blue-container").droppable({
+	accept: ".blue",
+	drop: function (e, postit) {
+		let counter = $(this).find("span").text();
+		if (postit.draggable.data("dropped") == false) {
 			counter++;
-		});
+			postit.draggable.data("dropped", true);
+		}
+		$(this).find("span").text(counter);
+	},
 
-		$('.blue-container').text(counter);
+	out: function (e, postit) {
+		let counter = $(this).find("span").text();
+		if (postit.draggable.data("dropped") == true) {
+			counter--;
+			postit.draggable.data("dropped", false);
+		}
+		$(this).find("span").text(counter);
+	}
+});
 
+$(".red-container").droppable({
+	accept: ".red",
+	drop: function (e, postit) {
+		let counter = $(this).find("span").text();
+		if (postit.draggable.data("dropped") == false) {
+			counter++;
+			postit.draggable.data("dropped", true);
+		}
+		$(this).find("span").text(counter);
+	},
 
-		console.log(ui.draggable);
+	out: function (e, postit) {
+		let counter = $(this).find("span").text();
+		if (postit.draggable.data("dropped") == true) {
+			counter--;
+			postit.draggable.data("dropped", false);
+		}
+		$(this).find("span").text(counter);
+	}
+});
+
+$(".yellow-container").droppable({
+	accept: ".yellow",
+	drop: function (e, postit) {
+		let counter = $(this).find("span").text();
+		if (postit.draggable.data("dropped") == false) {
+			counter++;
+			postit.draggable.data("dropped", true);
+		}
+		$(this).find("span").text(counter);
+	},
+
+	out: function (e, postit) {
+		let counter = $(this).find("span").text();
+		if (postit.draggable.data("dropped") == true) {
+			counter--;
+			postit.draggable.data("dropped", false);
+		}
+		$(this).find("span").text(counter);
 	}
 });
