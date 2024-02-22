e5b4e183e86baa52a4eb03420e43e4507ea26779
AdriaMelero1 <adriamelero1@gmail.com>
2024-02-22 21:15:09 +0100

    Localstorage

--- ud7/ej3/Post.js
+++ ud7/ej3/Post.js
@@ -1,11 +1,12 @@
 export default {
 	name: 'post',
 	props: ['post'],
+	emits: ['delete-post', 'edit-post'],
 	methods: {
-		deletePost: function(post) {
-			this.$emit('delete-post', post);
-		},
-		editPostBtn: function(post) {
+		// deletePost: function(post) {
+		// 	this.$emit('delete-post', post);
+		// },
+		editPost: function(post) {
 			this.$emit('edit-post', post);
 		}
 	},
@@ -16,8 +17,9 @@ export default {
 		<p>Author: {{post.author}}</p>
 		<img :src="post.postImageUrl">
 		<p id="creationDate">Created: {{post.creationDate.split('T')[0]}}</p>
-		<button v-on:click="deletePost(post)">Delete Post</button>
-		<button v-on:click="editPostBtn(post)">Edit Post</button>
+
+		<button v-on:click="$emit('delete-post', post)">Delete Post</button>
+		<button v-on:click="editPost(post)">Edit Post</button>
 		<button v-on:click="">Schedule Publish</button>
 	</div>
 	`,
